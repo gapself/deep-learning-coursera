e370554b1245280680522111b737a2a8c3911975
gabipaszk <gabriela.paszkiewicz@gmail.com>
2021-05-19 20:55:33 +0200

    Broadcasting example

---
+++ 2w/broadcasting.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+fruits = ['apples', 'beef', 'eggs', 'potatoes']
+carb = [56.0, 0.0, 4.4, 68.0]
+protein = [1.2, 104.0, 52.0, 8.0]
+fat = [1.8, 135.0, 99.0, 0.9]
+
+A = np.array([carb, protein, fat])
+print(A)
+
+cal = A.sum(axis=0)  # axis=0 means sum up vertically
+print(cal)
+
+percentage = 100 * A / cal.reshape(1, 4)  # .reshape(1,4) optional, good practice
+print(percentage)
